a47faa3f033b51af7552b13ccd405abe2e8118b6
zxystd <1051244836@qq.com>
2021-08-29 23:20:22 +0800

    remove duplicate malloc functions.

--- itl80211/linux/kernel.h
+++ itl80211/linux/kernel.h
@@ -238,33 +238,6 @@ static inline void* kzalloc(size_t size)
     return ret;
 }
 
-static inline void* iwh_malloc(vm_size_t len) {
-    void *addr = IOMalloc(len + sizeof(vm_size_t));
-    if (addr == NULL)
-        return NULL;
-    
-    *((vm_size_t*)addr) = len;
-    return (void*)((uint8_t*)addr + sizeof(vm_size_t));
-}
-
-static inline void* iwh_zalloc(vm_size_t len) {
-    void* addr = iwh_malloc(len);
-    if (addr == NULL)
-        return NULL;
-    
-    bzero(addr, len);
-    return addr;
-}
-
-static inline void iwh_free(void* ptr) {
-    if (!ptr)
-        return;
-    
-    void* start_addr = (void*)((uint8_t*)ptr - sizeof(vm_size_t));
-    vm_size_t block_size = *((vm_size_t*)start_addr) + sizeof(vm_size_t);
-    IOFree(start_addr, block_size);
-}
-
 
 
 static inline int atomic_dec_and_test(volatile SInt32 * addr)
