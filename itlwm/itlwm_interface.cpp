5c9abb16dd24210ba5cd24618136d4fc54a809bb
zxystd <1051244836@qq.com>
2022-06-21 22:16:39 +0800

    itlwm: Use default MTU.

--- itlwm/itlwm_interface.cpp
+++ itlwm/itlwm_interface.cpp
@@ -19,19 +19,7 @@ OSDefineMetaClassAndStructors( itlwm_interface, IOEthernetInterface )
 
 bool itlwm_interface::init(IONetworkController *controller)
 {
-    if (!super::init(controller))
-        return false;
-    setMaxTransferUnit(1482);
-    return true;
-}
-
-bool itlwm_interface::setMaxTransferUnit(UInt32 mtu) {
-    IOLog("itlwm setting MTU to %d\n", mtu);
-    if (mtu > ETHERNET_MTU) {
-        return false;
-    }
-    super::setMaxTransferUnit(mtu);
-    return true;
+    return super::init(controller);
 }
 
 void itlwm_interface::free()
--- itlwm/itlwm_interface.hpp
+++ itlwm/itlwm_interface.hpp
@@ -17,8 +17,6 @@
 
 #include <IOKit/network/IOEthernetInterface.h>
 
-#define ETHERNET_MTU            1500
-
 class itlwm_interface : public IOEthernetInterface {
     OSDeclareDefaultStructors( itlwm_interface )
     
@@ -28,8 +26,6 @@ public:
 protected:
     
     virtual void free() override;
-    
-    virtual bool setMaxTransferUnit(UInt32 mtu) override;
 };
 
 #endif /* itlwm_interface_hpp */
