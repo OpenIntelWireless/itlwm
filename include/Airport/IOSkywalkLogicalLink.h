7db75d934e8322653276328e0d8f44d9d4331696
zxystd <1051244836@qq.com>
2023-06-12 18:30:35 +0800

    add IOSkywalkLogicalLink class placeholder.

---
+++ include/Airport/IOSkywalkLogicalLink.h
@@ -0,0 +1,20 @@
+//
+//  IOSkywalkLogicalLink.h
+//  itlwm
+//
+//  Created by qcwap on 2023/6/12.
+//  Copyright © 2023 钟先耀. All rights reserved.
+//
+
+#ifndef IOSkywalkLogicalLink_h
+#define IOSkywalkLogicalLink_h
+
+class IOSkywalkLogicalLink : public IOService {
+    OSDeclareAbstractStructors(IOSkywalkLogicalLink)
+    
+public:
+    
+public:
+};
+
+#endif /* IOSkywalkLogicalLink_h */
